1bfc4290854769ebafafd5888c3557a252bfa71d
Yaroslav Tkach <hrom233@hotmail.com>
2022-01-31 14:01:41 +0200

    Completed Task 2
    Created a gallery with simplelightbox library.

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
+
+// Create a markup
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+  })
+  .join('');
+
+list.insertAdjacentHTML('afterbegin', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlayOpacity: 1,
+});
